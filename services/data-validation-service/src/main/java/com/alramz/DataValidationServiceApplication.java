bc6713716a537c52d9ee4ea7bb7b9a565b415fb5
gaurav9969351313 <gaurav.cdac16@gmail.com>
2026-07-29 22:33:11 +0530

    Test 2

--- services/data-validation-service/src/main/java/com/alramz/DataValidationServiceApplication.java
+++ services/data-validation-service/src/main/java/com/alramz/DataValidationServiceApplication.java
@@ -8,7 +8,7 @@ public class DataValidationServiceApplication {
 
 	// http://localhost:5001/api/v1/info
 	public static void main(String[] args) {
-		System.out.println("Gaurav ");
+		System.out.println("Gaurav Ta");
 		SpringApplication.run(DataValidationServiceApplication.class, args);
 	}
 
