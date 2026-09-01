d89851249c61a9b5229b9a403472e02768f71199
Gaurav Talele <epostgauravtalele@gmail.com>
2026-09-01 11:26:58 +0530

    Update main.tf

--- infra/modules/platform-identity-roles/main.tf
+++ infra/modules/platform-identity-roles/main.tf
@@ -14,8 +14,20 @@ variable "role_assignments" {
   default = {}
 }
 
+data "azurerm_role_assignment" "existing" {
+  for_each = var.role_assignments
+
+  scope                = each.value.scope
+  role_definition_name = each.value.role
+  principal_id         = var.identity_principal_id
+}
+
 resource "azurerm_role_assignment" "dynamic" {
-  for_each             = var.role_assignments
+  for_each = {
+    for k, v in var.role_assignments : k => v
+    if try(data.azurerm_role_assignment.existing[k].id, null) == null
+  }
+
   scope                = each.value.scope
   role_definition_name = each.value.role
   principal_id         = var.identity_principal_id
